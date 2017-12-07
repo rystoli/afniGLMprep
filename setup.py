539b9cb83e71e8fa625560fc35ced2b6688e82b2
rystoli <rystoli@nyu.edu>
2017-12-06 23:55:04 -0500

    pip install

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='afniGLMprep',
+        version='0.0.1',
+        description='Automatic AFNI GLM script generation from BIDS format',
+        author='Ryan Stolier',
+        author_email='rystoli@nyu.edu',
+        url='https://github.com/rystoli/afniGLMprep',
+        packages=['afniglmprep']
+        )
+
